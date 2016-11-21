ef2364413ff2cdd5c4246dd4c43fa86ac1ec182b
Alexei <alexeim763@gmail.com>
2016-11-20 20:32:02 -0500

    Add upload button

--- src/header/index.js
+++ src/header/index.js
@@ -1,24 +1,29 @@
 var yo = require('yo-yo');
 var translate = require('../translate');
+var empty = require('empty-element');
 
 var el = yo`<nav class="header">
-  <div class="nav-wrapper">
-    <div class="container">
-      <div class="row">
-        <div class="col s12 m6 offset-m1">
-          <a href="/" class="brand-logo title-logo">CloneGram</a>
-        </div>
-        <div class="col s2 m6 push-s10 push-m10">
-          <a class="btn btn-large btn-flat dropdown-button" href="#" data-activates="drop-user">
-            <i class="fa fa-user" aria-hidden="true"></i>
-          </a>
-          <ul id="drop-user" class="dropdown-content">
-            <li><a href="#">${translate.message('logout')}</a></li>
-          </ul>
+      <div class="nav-wrapper">
+        <div class="container">
+          <div class="row">
+            <div class="col s12 m6 offset-m1">
+              <a href="/" class="brand-logo title-logo">CloneGram</a>
+            </div>
+            <div class="col s2 m6 push-s10 push-m10">
+              <a href="#" class="btn btn-large btn-flat dropdown-button" data-activates="drop-user">
+                <i class="fa fa-user" aria-hidden="true"></i>
+              </a>
+              <ul id="drop-user" class="dropdown-content">
+                <li><a href="#">${translate.message('logout')}</a></li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-</nav>`;
+    </nav>`;
 
-var main = document.getElementById('header-container').appendChild(el);
+module.exports = function header (ctx, next) {
+  var container = document.getElementById('header-container')
+  empty(container).appendChild(el);
+  next();
+}
